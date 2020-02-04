b1db34873c8cf829b7dbf4c6ff5dbe2d01688f23
Will Ware <wware@alum.mit.edu>
2020-02-04 08:51:20 -0500

    progress

--- x.c
+++ x.c
@@ -1,20 +1,47 @@
 // https://www.cprogramming.com/tutorial/shared-libraries-linux-gcc.html
 // https://dbader.org/blog/python-ctypes-tutorial
 // https://dbader.org/blog/python-ctypes-tutorial-part-2
-// gcc -Wall -c x.c ; gcc -shared -o x.so x.o
+// gcc -Wall -O2 -c x.c ; gcc -shared -o x.so x.o
+
+/* Passing structs to functions by value in C - bad idea?
+   https://stackoverflow.com/questions/161788 */
 
 #include <stdio.h>
 
-typedef struct vector {
+typedef struct {
 	double x, y, z;
 } Vector;
 
-double dot(struct vector *v1, struct vector *v2)
+typedef struct {
+    int v1;
+    int v2;
+	Vector end1;
+	Vector end2;
+} Edge;
+
+Vector linear(double a, Vector *v1, double b, Vector *v2)
 {
-	printf("%lf\n", v1->x);
-	return (
-		v1->x * v2->x +
-		v1->y * v2->y +
-		v1->z * v2->z
-	);
+    Vector v = {
+        a * v1->x + b * v2->x,
+        a * v1->y + b * v2->y,
+        a * v1->z + b * v2->z
+    };
+    return v;
+}
+
+double dot(Vector *v1, Vector *v2)
+{
+	return v1->x * v2->x +
+	       v1->y * v2->y +
+		   v1->z * v2->z;
+}
+
+Vector cross(Vector *v1, Vector *v2)
+{
+    Vector v = {
+        v1->y * v2->z - v1->z * v2->y,
+        v1->z * v2->x - v1->x * v2->z,
+        v1->x * v2->y - v1->y * v2->x
+    };
+    return v;
 }
--- x.py
+++ x.py
@@ -1,24 +1,103 @@
-from ctypes import *
+import ctypes
 
-class POINT(Structure):
-    _fields_ = ("x", c_double), ("y", c_double), ("z", c_double)
+lib = ctypes.CDLL("./x.so")
 
-class EDGE(Structure):
-    _fields_ = [("v1", c_int),
-                ("v2", c_int),
-                ("end1", POINT),
-                ("end2", POINT)]
 
-lib = CDLL("./x.so")
-print lib.dot
+class Vector(ctypes.Structure):
+    _fields_ = ("x", ctypes.c_double), ("y", ctypes.c_double), ("z", ctypes.c_double)
 
-VEC = lib.Vector
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-func = lib.dot
-func.argtypes = POINT, POINT
+        def wrap_function(lib, funcname, restype, argtypes):
+            func = lib.__getattr__(funcname)
+            func.restype = restype
+            func.argtypes = argtypes
+            return func
 
-u = POINT()
-v = POINT()
-u.x, u.y, u.z = 1, 2, 3
-v.x, v.y, v.z = 2, 3, 5
+        self._dot_func = wrap_function(
+            lib, 'dot', ctypes.c_double,
+            [ctypes.POINTER(Vector), ctypes.POINTER(Vector)]
+        )
+        self._cross_func = wrap_function(
+            lib, 'cross', Vector,
+            [ctypes.POINTER(Vector), ctypes.POINTER(Vector)]
+        )
+        self._linear_func = wrap_function(
+            lib, 'linear', Vector,
+            [ctypes.c_double, ctypes.POINTER(Vector), ctypes.c_double, ctypes.POINTER(Vector)]
+        )
 
+    def __repr__(self):
+        return '({0}, {1}, {2})'.format(self.x, self.y, self.z)
+
+    def __neg__(self, other):
+        return self._linear_func(-1., self, 0., self)
+
+    def __add__(self, other):
+        return self._linear_func(1., self, 1., other)
+
+    def __sub__(self, other):
+        return self._linear_func(1., self, -1., other)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return self._linear_func(other, self, 0., self)
+        elif isinstance(other, Vector):
+            return self.dot(other)
+        else:
+            raise TypeError(type(other).__name__)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def scale(self, k):
+        return self._linear_func(k, self, 0., self)
+
+    def dot(self, other):
+        return self._dot_func(self, other)
+
+    def cross(self, other):
+        return self._cross_func(self, other)
+
+    @classmethod
+    def tests(cls):
+        u = cls(1, 2, 3)
+        v = cls(4, 5, 6)
+        print 9 * u
+        print u * 7
+        print u * v
+        print u + v
+        print v - u
+        print u.cross(v)
+
+
+class Edge(ctypes.Structure):
+    _fields_ = [("v1", ctypes.c_int),
+                ("v2", ctypes.c_int),
+                ("end1", Vector),
+                ("end2", Vector)]
+
+
+Vector.tests()
+
+class Shape(object):
+    # Base shape is a tetrahedron, this is misshapen
+    vertices = (
+        Vector(1, 0, -1),
+        Vector(-1, 0, -1),
+        Vector(1, 0, 1),
+        Vector(-1, 0, 1)
+    )
+
+    edges = (
+        (0, 1), (0, 2), (0, 3),
+        (1, 2), (1, 3), (2, 3)
+    )
+
+
+
+s = Shape()
+print s.vertices[1]
